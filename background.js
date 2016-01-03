a382ec661942a3c270d316d0a305544da4697318
masaibar <orebatemoko@gmail.com>
2016-01-03 22:41:58 +0900

    非同期ではexecCommand実行できなさそうなところで手詰まり

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 var title = null;
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	//読込中の場合はタイトル等取れない場合がある
+	if (changeInfo.status === "loading") {
+		return;
+	}
+
 	if (tab.url.match(/www\.kasi-time\.com\/item-[0-9]*\.html$/) != null) {
 		//URLが歌詞タイムの歌詞ページだったらアイコンをアドレスバーに表示する
 		chrome.pageAction.show(tabId);
@@ -13,29 +18,30 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	} else if (tab.url.match(/j-lyric\.net\/artist\/.*\/.*\.html$/) != null) {
 		//URLがJ-Lyric.netの歌詞ページだったらアイコンをアドレスバーに表示する
 		chrome.pageAction.show(tabId);
-		
+
 		chrome.tabs.executeScript(null,
 			{"code":"document.getElementsByClassName('caption')[0].getElementsByTagName('h2')[0].innerHTML"
 			}, function(result) {
 				title = result;
 			});
 	} else if (tab.url.match(/kashinavi\.com\/song_view\.html\?[0-9]*$/) != null) {
-        //URLが歌詞ナビの歌詞ページだったらアイコンをアドレスバーに表示する
-        chrome.pageAction.show(tabId);
+		//URLが歌詞ナビの歌詞ページだったらアイコンをアドレスバーに表示する
+		chrome.pageAction.show(tabId);
 
-        chrome.tabs.executeScript(null,
-            {"code":"document.getElementsByTagName('title')[0].innerHTML.replace(/ - 歌詞ナビ/g, '')"
-            }, function(result) {
-                title = result;
-            });
+		chrome.tabs.executeScript(null,
+				{"code":"document.getElementsByTagName('title')[0].innerHTML.replace(/ - 歌詞ナビ/g, '')"
+				}, function(result) {
+					title = result;
+				});
+
 	} else if (tab.url.match(/www\.uta-net\.com\/song\/[0-9]*/) != null) {
-        //URLがUta-Netの歌詞ページだったらアイコンをアドレスバーに表示する
-        chrome.pageAction.show(tabId);
+		//URLがUta-Netの歌詞ページだったらアイコンをアドレスバーに表示する
+		chrome.pageAction.show(tabId);
 
-        chrome.tabs.executeScript(null,
-            {"code":"document.getElementsByClassName('prev_pad')[0].innerHTML"
-            }, function(result) {
-                title = result;
-            });
-    }
+		chrome.tabs.executeScript(null,
+				{"code":"document.getElementById('view_kashi').getElementsByTagName('h2')[0].innerHTML"
+				}, function(result) {
+					title = result;
+				});
+	}
 });
