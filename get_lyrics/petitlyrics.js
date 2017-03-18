272cba6f721e8e9d112e52f8469a675f15d0a4ca
minodow <minodow@gmail.com>
2017-03-18 14:15:02 +0900

    プチリリから歌詞を取得する処理を追加

--- get_lyrics/petitlyrics.js
+++ get_lyrics/petitlyrics.js
@@ -1,1 +1,31 @@
-no contents
+var lyrics=document.getElementById("lyrics").innerHTML.replace(/<br>/g, "").replace(/&amp;/g, "&");
+var title=document.getElementsByClassName('title-bar')[0].innerHTML;
+
+console.log(lyrics);
+
+var dummyDiv = document.createElement("div");
+dummyDiv.innerHTML = '<textarea id="copyTextarea">'+lyrics+'</textarea>';
+
+//ページが下にスクロールされないようにheaderの下に擬似的にtextareaを作る
+var parentObject = document.getElementsByClassName("footer_menu")[0];
+parentObject.appendChild(dummyDiv);
+
+//コピーするためにテキストエリアを選択状態にする
+var copyTextarea=document.getElementById("copyTextarea");
+copyTextarea.select();
+copyTextarea.focus();
+
+//クリップボードへの書き込みを実行、成功true/失敗falseが戻る
+var result = document.execCommand("Cut");
+
+//ダミーのtextareaが表示されないようにする
+copyTextarea.style.display="none";
+
+if(result == true) {
+	alert("\""+title+ "\" の歌詞を\nクリップボードにコピーしました");
+} else {
+	alert("コピーに失敗しました");
+}
+
+//コピーしたらダミーのテキストエリアを消す
+parentObject.removeChild(dummyDiv);
